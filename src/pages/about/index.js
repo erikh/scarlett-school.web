8d02f8d3593564ed85264929623f65b7e609e2b1
Erik Hollensbe <git@hollensbe.org>
2023-03-18 15:29:37 -0700

    About page
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,16 @@
 import Typography from "@mui/material/Typography";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import preval from "preval.macro";
 
 export default function About() {
-  return <Typography>About Page!</Typography>;
+  const about = preval`
+    const fs = require("node:fs");
+    module.exports = fs.readFileSync("source/papers/rules-and-ethos.md", "utf8");
+  `;
+  return (
+    <Typography sx={{ p: "2em" }}>
+      <ReactMarkdown remarkPlugins={[[remarkGfm]]}>{about}</ReactMarkdown>
+    </Typography>
+  );
 }
